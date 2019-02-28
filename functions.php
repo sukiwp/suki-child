a78a02dcda1c743b522d3bfe14bc8247bd1f789e
David Rozando <david.rozando@gmail.com>
2019-02-28 20:55:23 +0700

    Add parent mods mode

--- functions.php
+++ functions.php
@@ -11,9 +11,18 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
+ * By default, when migrating to Child Theme, your existing configuration on Parent Theme options are not inherited to Child Theme.
+ * It's because WordPress treats Parent Theme configuration and Child Theme configuration as separate data.
+ *
+ * Set the constant to `true` to make this Child Theme inherit Parent Theme configuration data.
+ */
+define( 'SUKI_CHILD_USE_PARENT_MODS', false );
+
+/**
  * Enqueue Child Theme CSS.
  * Attached on 'wp_enqueue_scripts' hook with priority set to 20 to make sure all parent theme CSS has been loaded first.
- * Comment out the block below if you don't have additional CSS on style.css.
+ *
+ * Remove or comment out this block below if you don't have additional CSS on style.css.
  */
 function suki_child_enqueue_styles() {
 	wp_enqueue_style( 'suki-child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'suki' ), wp_get_theme()->get( 'Version' ) );
